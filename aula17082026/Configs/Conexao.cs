182f1cb6bfdb19d78fbbbf14577edd2dadd015a0
yasminpereirareis29-star <yasminpereirareis29@gmail.com>
2026-08-24 09:25:06 -0400

    tarefa

---
+++ aula17082026/Configs/Conexao.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace aula17082026.Configs
+{
+    public class Conexao
+    {
+
+
+
+        private readonly string _connectionString;
+        public Conexao(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("MySqlConnection") ?? "";
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            var conn = new MySqlConnection(_connectionString);
+            conn.Open();
+            return conn;
+        }
+
+        public MySqlCommand CreateCommand(string query, MySqlConnection? conn = null)
+        {
+            conn ??= GetConnection();
+            return new MySqlCommand(query, conn);
+        }
+
+
+    }
+}
---
+++ aula17082026/Program.cs
@@ -0,0 +1,31 @@
+using aula17082026.Components;
+using aula17082026.Configs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+//configuração da conexão com o banco de dados MYSQL
+builder.Services.AddScoped<Conexao>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
