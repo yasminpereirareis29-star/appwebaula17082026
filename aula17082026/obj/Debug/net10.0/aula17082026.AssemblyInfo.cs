a19f677f46d3c470110253d47a78cf77c48e15df
yasminpereirareis29-star <yasminpereirareis29@gmail.com>
2026-08-24 09:40:26 -0400

    commit

--- aula17082026/obj/Debug/net10.0/aula17082026.AssemblyInfo.cs
+++ aula17082026/obj/Debug/net10.0/aula17082026.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula17082026")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+182f1cb6bfdb19d78fbbbf14577edd2dadd015a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula17082026")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula17082026")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
